cc0e0f7021a9a38e184477e699c37246100efc1f
ngoquangdatjpnd <66765392+ngoquangdatjpnd@users.noreply.github.com>
2022-05-10 17:26:10 +0700

    test

--- modules/ShowWeather.tsx
+++ modules/ShowWeather.tsx
@@ -7,6 +7,7 @@ interface props {
     codeIcon : string,
     des : string
 }
+
 const ShowWeather = (props : props) => {
 
     const {temp,nameCity,code,codeIcon,des} = props;
@@ -14,10 +15,10 @@ const ShowWeather = (props : props) => {
     const textTemp = temp + " *C";
 
     const checkNightDay = () => {
-        return codeIcon.slice(3)
+        return codeIcon.slice(2)
     }
     console.log(checkNightDay())
-    if (checkNightDay() == "d" && parseInt(code) >=700) {
+    if (codeIcon.slice(3) === "d" && parseInt(code) >=700) {
         return (
             <ImageBackground style={styles.image} source={require('../assets/sunny.jpg')}>
             <Text style={styles.text}>{nameCity}</Text>
@@ -26,13 +27,13 @@ const ShowWeather = (props : props) => {
         </ImageBackground>
         )
     }
-    else if (checkNightDay() == "d" && parseInt(code) <=700) { return (
+    else if (checkNightDay() === "d" && parseInt(code) <=700) { return (
         <ImageBackground style={styles.image} source={require('../assets/cloudy.jpeg')}>
         <Text style={styles.text}>{nameCity}</Text>
         <Text style={styles.text2}>{textTemp} </Text>
         <Text style={styles.text3}>{des}</Text>
         </ImageBackground>)
-    } else if (checkNightDay() == "n" && parseInt(code) <=700) { return (
+    } else if (checkNightDay() === "n" && parseInt(code) <=700) { return (
         <ImageBackground style={styles.image} source={require('../assets/rainy.jpg')}>
         <Text style={styles.text}>{nameCity}</Text>
         <Text style={styles.text2}>{textTemp} </Text>
